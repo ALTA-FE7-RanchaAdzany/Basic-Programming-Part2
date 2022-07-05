4b93014e6af74f97cbea983094657c0ee9ac1cf1
ray34-cyber <rancha.adzany1@gmail.com>
2022-07-05 22:31:23 +0800

    menambahkan problem 1,2,4,5 pada part 3

--- Part3/problem2/problem2.js
+++ Part3/problem2/problem2.js
@@ -1,7 +1,24 @@
 function drawXYZ(high) {
   // Your Code Here
+  for (let i = 1; i <= high; i++) {
+    let output = "";
+    for (let j = i; j < i + high; j++) {
+      if (j % 3 == 0) {
+        output += "X";
+      } else if (j % 2 == 0) {
+        output += "Z";
+      } else {
+        output += "Y";
+      }
+    }
+    console.log(output);
+    output = "";
+  }
 }
 
-console.log(drawXYZ(3));
-console.log(drawXYZ(5));
-console.log(drawXYZ(1));
+drawXYZ(3);
+drawXYZ(5);
+drawXYZ(1);
+// console.log(drawXYZ(3));
+// console.log(drawXYZ(5));
+// console.log(drawXYZ(1));
